9ee8d5bb14ffc48b27e739cd6ad077c7e48997d1
4kreesh <muthukrishnan.h@gmail.com>
2023-11-28 18:39:28 -0800

    Pie chart and second query added

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from config import username, password, hostname, port, db
 
 app = Flask(__name__)
 
-
+print(username, password, hostname, port, db)
+#engine = create_engine('postgresql+psycopg2://postgres:Postgres23@localhost:5432/Project_3_recalls')
 engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{db}')
 
 @app.route("/")
@@ -24,6 +25,23 @@ def q1():
     df = pd.read_sql(query, conn)
     # returns a dictionary of lists
     return df.reset_index().to_dict(orient='list')
+    #return df.to_json(orient='split')
+
+
+@app.route('/api/v1.0/q2')
+def q2():
+    conn = engine.connect()
+    query2 = "select manufacturer, sum(cast(potentially_affected as float)) as sum_pa \
+                from recalls \
+                where manufacturer in ('Mercedes-Benz USA, LLC', \
+                'BMW of North America, LLC',\
+                'Porsche Cars North America, Inc.',\
+                'Honda (American Honda Motor Co.)', \
+                'Volkswagen Group of America, Inc.') \
+                group by manufacturer order by sum_pa desc"
+    df2 = pd.read_sql(query2, conn)
+    # returns a dictionary of lists
+    return df2.reset_index().to_dict(orient='list')
     #return df.to_json(orient='split')
 
 if __name__ == "__main__":
--- static/js/app.js
+++ static/js/app.js
@@ -27,7 +27,56 @@ function init(){
         Plotly.newPlot('bar',data,layout);
 
     });
+// Second query and Pie chart 
+d3.json(`${url}/api/v1.0/q2`).then(response => {
+        console.log(response);
+        let manufacturers = response.manufacturer;
+        let totalAffected = response.sum_pa;
+        console.log('manufacturers: ', manufacturers,totalAffected);
+        //const DATA_COUNT = 5;
+        
+        
+        // Data info
+        const customColors = [
+            'rgba(255, 99, 132, 0.8)',
+            'rgba(54, 162, 235, 0.8)',
+            'rgba(255, 206, 86, 0.8)',
+            'rgba(75, 192, 192, 0.8)',
+            'rgba(153, 102, 255, 0.8)',
+            'rgba(255, 159, 64, 0.8)',
+        ];
+        const data = {
+        labels: manufacturers,
+        datasets: [
+            {
+            label: 'Potentially affected Manufacturers',
+            data: totalAffected,
+            backgroundColor: customColors ,
+            borderColor: customColors,
+            borderWidth: 1
+            }
+        ]
+        };
 
+        var ctx = document.getElementById('pie').getContext('2d');
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: data,
+            options: {
+                plugins: {
+                  title: {
+                    display: true,
+                    text: 'Potentially affected Manufacturers',
+                    fontSize: 36
+                  }
+                }
+            },
+        });
+
+      
+
+    });
 };
+
 
 init();
